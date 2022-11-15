beaafe2761e14336e40074fb5ac3e893e8f8d6a1
Gabriel Reis <gabrielgrds@outlook.com>
2022-11-15 14:23:34 -0400

    midi pronto

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encotrado');
+        //alert('Elemento ou seletor não encotrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
